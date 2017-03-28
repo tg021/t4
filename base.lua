8d93d39e17a5d4a2345679289622cf5256ca1f10
tg021 <Tgs4jj4d@gmail.com>
2017-03-28 22:55:25 +0430

    Update base.lua

--- base.lua
+++ base.lua
@@ -8,16 +8,14 @@ function vardump(value)
 end
 
 function reload()
-  loadfile("tabchi.lua")()
-end
-
+   tabchi = dofile("tabchi.lua")end
 function dl_cb (arg, data)
 end
 
 reload()
 
 function tdcli_update_callback(data)
-  update(data, tabchi_id)
+  tabchi.update(data, tabchi_id)
   if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
     reload()
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
